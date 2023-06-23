ad37157f96272f25acbb3539df01c59c6a7bd151
alishaahuja <137496042+alishaahuja@users.noreply.github.com>
2023-06-23 16:34:55 +0530

    Add files via upload

---
+++ metaquery.py
@@ -0,0 +1,26 @@
+import json
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.compute import ComputeManagementClient
+
+def get_azure_instance_metadata(key=None):
+    metadata = {}
+
+    # Retrieve the instance metadata
+    try:
+        credential = DefaultAzureCredential()
+        client = ComputeManagementClient(credential, subscription_id='<Your Azure Subscription ID>')
+        vm = client.virtual_machines.get('<Your Resource Group Name>', '<Your VM Name>')
+        metadata = vm.__dict__['_attribute_map']
+    except Exception as e:
+        print(f"Error retrieving Azure instance metadata: {str(e)}")
+
+    # Filter the metadata based on the provided key
+    if key is not None:
+        metadata = {key: metadata.get(key)}
+
+    # Return the JSON-formatted output
+    return json.dumps(metadata, indent=4)
+
+# Example usage:
+azure_metadata = get_azure_instance_metadata()
+print(azure_metadata)

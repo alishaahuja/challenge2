107128971c163318cf098d7d0c86549517e3724d
alishaahuja <137496042+alishaahuja@users.noreply.github.com>
2023-06-24 18:02:18 +0530

    Update metaquery.py

--- metaquery.py
+++ metaquery.py
@@ -1,8 +1,8 @@
 import os
 import json
-from azure_metadata import AzureMetadata
-#from azure.identity import DefaultAzureCredential
-#from azure.mgmt.compute import ComputeManagementClient
+
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.compute import ComputeManagementClient
 
 
 
@@ -10,14 +10,22 @@ managed_identity_id = os.environ.get('MANAGED_IDENTITY_ID')
 
 
 def get_metadata_value(key):
-    metadata = AzureMetadata().compute
-    if key in metadata:
-        return metadata[key]
+    credential = DefaultAzureCredential()
+    compute_client = ComputeManagementClient(credential, subscription_id)
+
+    instance = compute_client.virtual_machines.get(resource_group_name, vm_name, expand="instanceView")
+
+    if key in instance.additional_properties:
+        return instance.additional_properties[key]
     else:
         return None
 
 # Example usage
+subscription_id = "cfd0f032-3c46-4727-8281-a17e0ec067d8"
+resource_group_name = "Alisha-tech-challenge"
+vm_name = "your-vm-name"
 data_key = "vmId"
+
 value = get_metadata_value(data_key)
 
 if value is not None:
@@ -25,4 +33,3 @@ if value is not None:
     print(json_output)
 else:
     print(f"Metadata key '{data_key}' not found.")
-
